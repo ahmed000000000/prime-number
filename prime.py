18b34058cda7e2bd26ac0546d214350f99dc5f24
ahmed000000000 <48419393+ahmed000000000@users.noreply.github.com>
2019-03-17 14:49:31 -0600

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,15 +1,12 @@
-num = int(input("number: "))
-if num>1:
-  for i in range(2,num):
+num = int(input("enter a number: \n"))
+print("\n")
+if num == 2:
+    print("2 is not a prime number because it is divisible by 2")
+    
+for i in range(2,num):
     if num % i == 0:
-        print(num, "is not a prime number")
+        print(num, "is not a prime number because it is divisble by", num//i)
         break
     else:
         print(num, "is a prime number")
         break
-else:
-  print(num, "is not a prime numer")
-  #if input is <1, not prime
-  #else checks to see if num divisible by any number greater than 2
-  # print prime
-  # else if num>1 and not divisible by any num, print that num is a prime number
